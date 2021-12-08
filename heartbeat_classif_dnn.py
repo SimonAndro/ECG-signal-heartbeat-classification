c3bc437e7e13c2430a23f50f496004b23558023b
simonandro <slemonla13@gmail.com>
2021-12-09 00:51:30 +0800

    creating the dataset

--- heartbeat_classif_dnn.py
+++ heartbeat_classif_dnn.py
@@ -56,7 +56,7 @@ total_by_category = dataframe.groupby('category').total.sum()
 # print(total_by_category)  # !debug, peek at the total by category
 
 fname = os.path.join(mit_bih_dir, str(record_names[0]))
-p_signal, labels, samples = load_ecg_signal(fname)
+p_signal, labels, samples, ok = load_ecg_signal(fname)
 label, total = np.unique(labels, return_counts=True)
 
 # for l, t in zip(label, total):
@@ -65,3 +65,90 @@ label, total = np.unique(labels, return_counts=True)
 # !debug, visualize part of the physical signal
 # plt.figure()
 # plt.plot(p_signal[:1000, 0])
+
+
+def make_dataset(record_names, num_sec, fs, abnormal):
+    # function for making dataset ignoring non-beats
+    # input:
+    # record_names - list of patients
+    # num_sec = number of seconds to include before and after the beat
+    # fs = frequency
+    # output:
+    #   X_all = signal (nbeats , num_sec * fs columns)
+    #   Y_all = binary is abnormal (nbeats, 1)
+    #   sym_all = beat annotation symbol (nbeats,1)
+
+    # initialize numpy arrays
+    num_cols = 2*num_sec * fs
+    X_all = np.zeros((1, num_cols))
+    Y_all = np.zeros((1, 1))
+    sym_all = []
+
+    # list to keep track of number of beats across patients
+    max_rows = []
+
+    for record_name in record_names:
+
+        fname = os.path.join(mit_bih_dir, str(record_name))
+
+        p_signal, labels, samples, ok = load_ecg_signal(fname)
+
+        if  not ok: # ok is false
+            continue
+
+        # grab the first signal, the MLII(Modified Lead II)
+        p_signal = p_signal[:, 0]
+
+        # make df to exclude the nonbeats
+        k_patient = pd.DataFrame({'labels': labels,
+                              'samples': samples})
+
+        k_patient = k_patient.loc[k_patient.labels.isin(non_normal_labels + ['N'])]
+
+        X, Y, sym = build_XY(p_signal, k_patient, num_cols, non_normal_labels)
+        sym_all = sym_all+sym
+        max_rows.append(X.shape[0])
+        X_all = np.append(X_all, X, axis=0)
+        Y_all = np.append(Y_all, Y, axis=0)
+
+    # drop the first zero row
+    X_all = X_all[1:, :]
+    Y_all = Y_all[1:, :]
+
+    # check sizes make sense
+    assert np.sum(
+        max_rows) == X_all.shape[0], 'number of X, max_rows rows messed up'
+    assert Y_all.shape[0] == X_all.shape[0], 'number of X, Y rows messed up'
+    assert Y_all.shape[0] == len(sym_all), 'number of Y, sym rows messed up'
+    return X_all, Y_all, sym_all
+
+
+def build_XY(p_signal, df_ann, num_cols, abnormal):
+    # this function builds the X,Y matrices for each beat
+    # it also returns the original symbols for Y
+
+    num_rows = len(df_ann)
+    X = np.zeros((num_rows, num_cols))
+    Y = np.zeros((num_rows, 1))
+    sym = []
+
+    # keep track of rows
+    max_row = 0
+    for atr_sample, atr_sym in zip(df_ann.samples.values, df_ann.labels.values):
+        left = max([0, (atr_sample - num_sec*fs)])
+        right = min([len(p_signal), (atr_sample + num_sec*fs)])
+        x = p_signal[left: right]
+        if len(x) == num_cols:
+            X[max_row, :] = x
+            Y[max_row, :] = int(atr_sym in abnormal)
+            sym.append(atr_sym)
+            max_row += 1
+    X = X[:max_row, :]
+    Y = Y[:max_row, :]
+    return X, Y, sym
+
+num_sec = 3
+fs = 360
+X_all, Y_all, label_all = make_dataset(record_names, num_sec, fs, non_normal_labels)
+
+print(X_all[:20], Y_all[:20], label_all[:20])
--- utilities.py
+++ utilities.py
@@ -2,12 +2,18 @@
 import wfdb
 import numpy as np
 
-def load_ecg_signal(file):
 
-    # load the ecg signal
-    record = wfdb.rdrecord(file)
-    # load annotation
-    annotation = wfdb.rdann(file, 'atr')
+def load_ecg_signal(fname):
+
+    try:  # ensure the file name given exists
+
+        # load the ecg signal
+        record = wfdb.rdrecord(fname)
+        # load annotation
+        annotation = wfdb.rdann(fname, 'atr')
+
+    except:
+        return [], [], [],False
 
     # asserting sampling frequency
     assert record.fs == 360, 'sample frequency is not 360Hz'
@@ -19,79 +25,4 @@ def load_ecg_signal(file):
     labels = annotation.symbol
     samples = annotation.sample
 
-    return p_signal, labels, samples
-
-
-def make_dataset(datapath, pts, num_sec, fs, abnormal):
-    # function for making dataset ignoring non-beats
-    # input:
-    # pts - list of patients
-    # num_sec = number of seconds to include before and after the beat
-    # fs = frequency
-    # output:
-    #   X_all = signal (nbeats , num_sec * fs columns)
-    #   Y_all = binary is abnormal (nbeats, 1)
-    #   sym_all = beat annotation symbol (nbeats,1)
-
-    # initialize numpy arrays
-    num_cols = 2*num_sec * fs
-    X_all = np.zeros((1, num_cols))
-    Y_all = np.zeros((1, 1))
-    sym_all = []
-
-    # list to keep track of number of beats across patients
-    max_rows = []
-
-    for pt in pts:
-        file = data_path + pt
-
-        p_signal, atr_sym, atr_sample = load_ecg(file)
-
-        # grab the first signal
-        p_signal = p_signal[:, 0]
-
-        # make df to exclude the nonbeats
-        df_ann = pd.DataFrame({'atr_sym': atr_sym,
-                              'atr_sample': atr_sample})
-        df_ann = df_ann.loc[df_ann.atr_sym.isin(abnormal + ['N'])]
-
-        X, Y, sym = build_XY(p_signal, df_ann, num_cols, abnormal)
-        sym_all = sym_all+sym
-        max_rows.append(X.shape[0])
-        X_all = np.append(X_all, X, axis=0)
-        Y_all = np.append(Y_all, Y, axis=0)
-    # drop the first zero row
-    X_all = X_all[1:, :]
-    Y_all = Y_all[1:, :]
-
-    # check sizes make sense
-    assert np.sum(
-        max_rows) == X_all.shape[0], 'number of X, max_rows rows messed up'
-    assert Y_all.shape[0] == X_all.shape[0], 'number of X, Y rows messed up'
-    assert Y_all.shape[0] == len(sym_all), 'number of Y, sym rows messed up'
-    return X_all, Y_all, sym_all
-
-
-def build_XY(p_signal, df_ann, num_cols, abnormal):
-    # this function builds the X,Y matrices for each beat
-    # it also returns the original symbols for Y
-
-    num_rows = len(df_ann)
-    X = np.zeros((num_rows, num_cols))
-    Y = np.zeros((num_rows, 1))
-    sym = []
-
-    # keep track of rows
-    max_row = 0
-    for atr_sample, atr_sym in zip(df_ann.atr_sample.values, df_ann.atr_sym.values):
-        left = max([0, (atr_sample - num_sec*fs)])
-        right = min([len(p_signal), (atr_sample + num_sec*fs)])
-        x = p_signal[left: right]
-        if len(x) == num_cols:
-            X[max_row, :] = x
-            Y[max_row, :] = int(atr_sym in abnormal)
-            sym.append(atr_sym)
-            max_row += 1
-    X = X[:max_row, :]
-    Y = Y[:max_row, :]
-    return X, Y, sym
+    return p_signal, labels, samples, True
